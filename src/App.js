aff3423a989063e8dce25861968bb8b684471d03
arnold <arnoldnderitu@gmail.com>
2023-01-23 16:37:21 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 export default function App() {
-  const post = {
-    id: 1,
-    text: 'Hello World',
-    timestamp: 'a few seconds ago',
-    author: {
-      username: 'jane.doe',
+  const posts = [
+    {
+      id: 1,
+      text: 'Hello World',
+      timestamp: 'a few seconds ago',
+      author: {
+        username: 'jane',
+      },
     },
-  }
+    {
+      id: 2,
+      text: 'Second post',
+      timestamp: 'a minute ago',
+      author: {
+        username: 'john',
+      },
+    },
+    {
+      id: 3,
+      text: 'Third post',
+      timestamp: 'an hour ago',
+      author: {
+        username: 'Jill',
+      },
+    }
+  ]
 
   return (
     <>
       <h1>Buddy</h1>
-      <p>
-        <b>{post.author.username}</b>&mdash;{post.timestamp}
-        <br />
-        {post.text}
-      </p>
+      {posts.length === 0 ?
+        <p>There are no blog posts</p>
+        :
+        posts.map(post => {
+          return (
+            <p key={post.id}>
+              <b>{post.author.username}</b> &mdash; {post.timestamp}
+              <br />
+              {post.text}
+            </p>
+          )
+        })
+      }
     </>
   );
 }
