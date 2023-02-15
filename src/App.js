4f8b223af4b6ef633dd6cd18912eb2ef113ae9bc
arnold <arnoldnderitu@gmail.com>
2023-02-15 15:17:43 +0300

    add page components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Posts from './components/Posts';
 import Body from './components/Body';
+import LoginPage from './pages/LoginPage';
+import FeedPage from './pages/FeedPage';
+import SignupPage from './pages/ExplorePage';
+import ExplorePage from './pages/ExplorePage';
 
 export default function App() {
 
 
   return (
     <Container fluid className='App'>
-      <Header />
-      <Body sidebar>
-        <Posts />
-      </Body>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<FeedPage />} />
+          <Route path='/explore' element={<ExplorePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }
