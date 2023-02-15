feb4d122e8b0a345e5e4762e74d14c9722002dfa
arnold <arnoldnderitu@gmail.com>
2023-02-15 13:15:05 +0300

    add posts component

--- src/App.js
+++ src/App.js
@@ -2,34 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Posts from './components/Posts';
 
 export default function App() {
-  const posts = [
-    {
-      id: 1,
-      text: 'Hello World',
-      timestamp: 'a few seconds ago',
-      author: {
-        username: 'jane',
-      },
-    },
-    {
-      id: 2,
-      text: 'Second post',
-      timestamp: 'a minute ago',
-      author: {
-        username: 'john',
-      },
-    },
-    {
-      id: 3,
-      text: 'Third post',
-      timestamp: 'an hour ago',
-      author: {
-        username: 'Jill',
-      },
-    }
-  ]
+
 
   return (
     <Container fluid className='App'>
@@ -38,19 +14,7 @@ export default function App() {
         <Stack direction='horizontal'>
           <Sidebar />
           <Container>
-            {posts.length === 0 ?
-              <p>There are no blog posts</p>
-              :
-              posts.map(post => {
-                return (
-                  <p key={post.id}>
-                    <b>{post.author.username}</b> &mdash; {post.timestamp}
-                    <br />
-                    {post.text}
-                  </p>
-                )
-              })
-            }
+            <Posts />
           </Container>
         </Stack>
       </Container>
---
+++ src/components/Posts.js
@@ -0,0 +1,46 @@
+export default function Posts() {
+    const posts = [
+        {
+            id: 1,
+            text: 'Hello World',
+            timestamp: 'a few seconds ago',
+            author: {
+                username: 'jane',
+            },
+        },
+        {
+            id: 2,
+            text: 'Second post',
+            timestamp: 'a minute ago',
+            author: {
+                username: 'john',
+            },
+        },
+        {
+            id: 3,
+            text: 'Third post',
+            timestamp: 'an hour ago',
+            author: {
+                username: 'Jill',
+            },
+        }
+    ]
+
+    return (
+        <>
+            {posts.length === 0 ?
+                <p>There are no blog posts</p>
+                :
+                posts.map(post => {
+                    return (
+                        <p key={post.id}>
+                            <b>{post.author.username}</b> &mdash; {post.timestamp}
+                            <br />
+                            {post.text}
+                        </p>
+                    )
+                })
+            }
+        </>
+    )
+}
